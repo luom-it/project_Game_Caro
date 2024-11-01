c3a439387f66200223c29a87822f536146f594b0
MinhChau16 <chauminh16102004@gmail.com>
2024-11-01 21:08:31 +0700

    update

--- project_Game_Caro/project_Game_Caro/obj/Debug/net8.0-windows/project_Game_Caro.AssemblyInfo.cs
+++ project_Game_Caro/project_Game_Caro/obj/Debug/net8.0-windows/project_Game_Caro.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project_Game_Caro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9423be74131ebacefbac22e32156fc7f47ea3847")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+641cc522f551ebc1bc6cb1af637ca277dbd3f0dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("project_Game_Caro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project_Game_Caro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
